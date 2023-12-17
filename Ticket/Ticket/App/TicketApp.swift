ce9f31e34d71232dc90a968c2e9b2516f4d68e63
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-12-17 16:39:32 +0200

    Initiate project

---
+++ Ticket/Ticket/App/TicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketApp.swift
+//  Ticket
+//
+//  Created by Mohamed Fawzi on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
