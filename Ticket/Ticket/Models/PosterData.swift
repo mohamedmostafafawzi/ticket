63486b22340991e8987cd836d466db54a1bede8c
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-12-17 16:58:50 +0200

    Add PosterData and TimeButton

---
+++ Ticket/Ticket/Models/PosterData.swift
@@ -0,0 +1,14 @@
+//
+//  PosterData.swift
+//  Ticket
+//
+//  Created by Mohamed Fawzi on 12/17/23.
+//
+
+import Foundation
+
+var posters1: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6"]
+
+var posters2: [String] = ["poster7", "poster8", "poster9", "poster10", "poster11", "poster12"]
+
+var posters3: [String] = ["poster13", "poster14", "poster15", "poster16", "poster17", "poster18"]
