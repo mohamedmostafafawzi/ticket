27b5b4db50954e67e174d20ccffe60546f97e273
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-12-18 17:40:00 +0200

    Add HomeView

---
+++ Ticket/Ticket/Views/HomeView.swift
@@ -0,0 +1,49 @@
+//
+//  HomeView.swift
+//  Ticket
+//
+//  Created by Mohamed Fawzi on 12/18/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State var animate: Bool = false
+    
+    var body: some View {
+        ZStack {
+            CircleBackground(color: Color("greenCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+                .task {
+                    withAnimation(.easeInOut(duration: 7).repeatForever()) {
+                        animate.toggle()
+                    }
+                }
+            
+            CircleBackground(color: Color("pinkCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+                .task {
+                    withAnimation(.easeInOut(duration: 4).repeatForever()) {
+                        animate.toggle()
+                    }
+                }
+            
+            VStack(spacing: 0.0) {
+                Text("Choose Movie")
+                    .fontWeight(.bold)
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color("backgroundColor"), Color("backgroundColor2")]), startPoint: .top, endPoint: .bottom)
+        )
+    }
+}
+
+#Preview {
+    HomeView()
+}
