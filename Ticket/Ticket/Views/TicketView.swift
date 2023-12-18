d74e445b3e93a00d82878574eab19b522bcf72d4
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2023-12-18 12:38:33 +0200

    Add TicketView

---
+++ Ticket/Ticket/Views/TicketView.swift
@@ -0,0 +1,52 @@
+//
+//  TicketView.swift
+//  Ticket
+//
+//  Created by Mohamed Fawzi on 12/18/23.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    @State var animate =  false
+    
+    
+    var body: some View {
+        ZStack {
+            CircleBackground(color: Color("greenCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+                .task {
+                    withAnimation(.easeInOut(duration: 7).repeatForever()) {
+                        animate.toggle()
+                    }
+                }
+            
+            CircleBackground(color: Color("pinkCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+            
+            VStack(spacing: 30) {
+                Text("Mobile Ticket")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                
+                Text("Once you buy a movie ticket simply scan the barcode to access to your movie.")
+                    .frame(maxWidth: 248)
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
+        }
+        .background(
+        LinearGradient(gradient: Gradient(colors: [Color("backgroundColor"), Color("backgroundColor2")]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+        )
+    }
+}
+
+#Preview {
+    TicketView()
+}
